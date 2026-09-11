ea7cfd63f85d2158fcb8e96ce2b55f2a4baff123
Tobias <tobiascodes12@gmail.com>
2026-09-11 16:30:10 -0400

    test: cover signal quality boundaries

--- test/test_core/test_main.cpp
+++ test/test_core/test_main.cpp
@@ -1,39 +1,46 @@
 #include <unity.h>
 #include <PlatformPocketCore.h>
 
-/**
- * @brief Document setUp.
- */
 void setUp()
 {
 }
 
-/**
- * @brief Document tearDown.
- */
 void tearDown()
 {
 }
 
-/**
- * @brief Document test strong signal.
- */
-void test_strong_signal()
+void test_signal_strong_range()
 {
-    TEST_ASSERT_EQUAL_STRING(
-        "STRONG",
-        getSignalLabel(-40)
-    );
+    TEST_ASSERT_EQUAL_STRING("STRONG", getSignalLabel(-40));
+    TEST_ASSERT_EQUAL_STRING("STRONG", getSignalLabel(-55));
 }
 
-/**
- * @brief Document main.
- */
+void test_signal_good_range()
+{
+    TEST_ASSERT_EQUAL_STRING("GOOD", getSignalLabel(-56));
+    TEST_ASSERT_EQUAL_STRING("GOOD", getSignalLabel(-67));
+}
+
+void test_signal_fair_range()
+{
+    TEST_ASSERT_EQUAL_STRING("FAIR", getSignalLabel(-68));
+    TEST_ASSERT_EQUAL_STRING("FAIR", getSignalLabel(-75));
+}
+
+void test_signal_weak_range()
+{
+    TEST_ASSERT_EQUAL_STRING("WEAK", getSignalLabel(-76));
+    TEST_ASSERT_EQUAL_STRING("WEAK", getSignalLabel(-100));
+}
+
 int main()
 {
     UNITY_BEGIN();
 
-    RUN_TEST(test_strong_signal);
+    RUN_TEST(test_signal_strong_range);
+    RUN_TEST(test_signal_good_range);
+    RUN_TEST(test_signal_fair_range);
+    RUN_TEST(test_signal_weak_range);
 
     return UNITY_END();
 }
